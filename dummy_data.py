cda93f0407a02857a950b761f6c39c0b91ce4b50
ryan-laytonmba <107505524+ryan-laytonmba@users.noreply.github.com>
2022-06-15 15:41:42 -0400

    Update dummy_data.py
    Signed-off-by: ryan-laytonmba <107505524+ryan-laytonmba@users.noreply.github.com>

--- dummy_data.py
+++ dummy_data.py
@@ -5,39 +5,74 @@ import streamlit as st
 def dummy_data(col_name, col_type, number_of_records = 1000):
     rec = []
     
-    if col_type == 'int':
-        for i in range(number_of_records):
-            rec.insert(i,fk.random_int(1,st.session_state.number_of_rows))
-    elif col_type.find('string') >= 0:
-        x = int(col_type[len('string'):])
-        for i in range(number_of_records):
-            rec.insert(i,fk.text(max_nb_chars = x))
-    elif col_type == 'id':
-        for i in range(number_of_records):
-            rec.insert(i,i+1)
-    elif col_type == 'phone number':
-        for i in range(number_of_records):
-            rec.insert(i,fk.phone_number())
-    elif col_type == 'address(full)':
-        for i in range(number_of_records):
-            rec.insert(i,fk.address())
-    elif col_type == 'address(street)':
-        for i in range(number_of_records):
-            rec.insert(i,fk.street_address())
-    elif col_type == 'name':
-        for i in range(number_of_records):
-            rec.insert(i,fk.name())
-    elif col_type == 'bool':
-        for i in range(number_of_records):
-            rec.insert(i,fk.pybool())
-    elif col_type == 'alphanumeric':
-        for i in range(number_of_records):
-            rec.insert(i,fk.password(length=40, special_chars=False))
-    elif col_type == 'datetime':
-        for i in range(number_of_records):
-            t = fk.time()
-            d = str(fk.date_between(start_date='-2y'))
-            rec.insert(i,d + ' ' + t)
+    if st.session_state.SQL_col == True:
+        if col_type == 'int':
+            for i in range(number_of_records):
+                rec.insert(i,fk.random_int(1,st.session_state.number_of_rows))
+        elif col_type.find('string') >= 0:
+            x = int(col_type[len('string'):])
+            for i in range(number_of_records):
+                rec.insert(i,"'" + fk.text(max_nb_chars = x) + "'")
+        elif col_type == 'id':
+            for i in range(number_of_records):
+                rec.insert(i,i+1)
+        elif col_type == 'phone number':
+            for i in range(number_of_records):
+                rec.insert(i,"'" + fk.phone_number() + "'")
+        elif col_type == 'address(full)':
+            for i in range(number_of_records):
+                rec.insert(i,"'" + fk.address() + "'")
+        elif col_type == 'address(street)':
+            for i in range(number_of_records):
+                rec.insert(i,"'" + fk.street_address() + "'")
+        elif col_type == 'name':
+            for i in range(number_of_records):
+                rec.insert(i,"'" + fk.name() + "'")
+        elif col_type == 'bool':
+            for i in range(number_of_records):
+                rec.insert(i,fk.pybool())
+        elif col_type == 'alphanumeric':
+            for i in range(number_of_records):
+                rec.insert(i,"'" + fk.password(length=40, special_chars=False) + "'")
+        elif col_type == 'datetime':
+            for i in range(number_of_records):
+                t = fk.time()
+                d = str(fk.date_between(start_date='-2y'))
+                rec.insert(i,"'" + d + ' ' + t + "'")
+    else:
+        if col_type == 'int':
+            for i in range(number_of_records):
+                rec.insert(i,fk.random_int(1,st.session_state.number_of_rows))
+        elif col_type.find('string') >= 0:
+            x = int(col_type[len('string'):])
+            for i in range(number_of_records):
+                rec.insert(i,fk.text(max_nb_chars = x))
+        elif col_type == 'id':
+            for i in range(number_of_records):
+                rec.insert(i,i+1)
+        elif col_type == 'phone number':
+            for i in range(number_of_records):
+                rec.insert(i,fk.phone_number())
+        elif col_type == 'address(full)':
+            for i in range(number_of_records):
+                rec.insert(i,fk.address())
+        elif col_type == 'address(street)':
+            for i in range(number_of_records):
+                rec.insert(i,fk.street_address())
+        elif col_type == 'name':
+            for i in range(number_of_records):
+                rec.insert(i,fk.name())
+        elif col_type == 'bool':
+            for i in range(number_of_records):
+                rec.insert(i,fk.pybool())
+        elif col_type == 'alphanumeric':
+            for i in range(number_of_records):
+                rec.insert(i,fk.password(length=40, special_chars=False))
+        elif col_type == 'datetime':
+            for i in range(number_of_records):
+                t = fk.time()
+                d = str(fk.date_between(start_date='-2y'))
+                rec.insert(i,d + ' ' + t)
     
     fill_data.update({col_name : rec})
 
@@ -102,14 +137,27 @@ def create_data():
 
             # print(fill_data)
 
+        
         df = pd.DataFrame(fill_data)
+        if st.session_state.SQL_col == True:
+            col = '('
+
+            for c in df.columns:
+                col = col + c + ', '
+
+            col = col[0:len(col)-2] + ')'
+
+            ins_st = 'insert into [TABLENAME] ' + col + ' values '
+
+            df['SQL'] = ins_st + '(' + df.astype(str).apply(','.join, axis=1) + ')'
+
 
         ct.markdown('###')
         ct.dataframe(df)
         ct.download_button('Download Data', df.to_csv(),file_name='dummy_data.csv')
 
 #page config
-st.set_page_config(page_title='Dummy Data Creator', layout='wide',page_icon='https://cdn-icons-png.flaticon.com/512/149/149206.png')
+st.set_page_config(page_title='Dummy Data Creator', layout='wide',page_icon='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLYWPfgwpyAuFhMoqNpLBOq_Ln_Gy5X_tUsA&usqp=CAU')
 
 #sidebar
 sd = st.sidebar
@@ -127,16 +175,17 @@ sd.write('- address(street) - street address only')
 sd.write('- alphanumeric - a random alphanumeric string')
 
 #main page
-col1, col2 = st.columns([.5,10])
-col1.image('https://cdn-icons-png.flaticon.com/512/149/149206.png')
+col1, col2 = st.columns([1,5])
+col1.image('https://cdn-icons-png.flaticon.com/512/149/149206.png',)
 col2.title('Dummy Data Creator')
 st.markdown('---')
 if 'col_num' not in st.session_state:
     st.session_state.col_num = 1
-col1, col2, col3 = st.columns([1,1,1])
+col1, col2, col3, col4= st.columns([1,1,.5,.5])
 col1.button('Add Column', on_click=col_count)
 col3.button('Create DF', on_click = create_data)
 col3.number_input(label='Number of Rows',key='number_of_rows', value=1000)
+col4.checkbox("Add SQL Insert Statement", value=True, key='SQL_col')
 st.markdown('###')
 ct = st.container()
 add_col()
